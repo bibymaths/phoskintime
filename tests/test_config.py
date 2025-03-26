bfa00c2392cc1b060263d2187d9d18224a12c35d
Abhinav Mishra <mishraabhinav36@gmail.com>
2025-03-26 22:44:26 +0100

    testing

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,6 @@
 from config.config import extract_config
 
+
 def test_extract_config_defaults():
     class Args:
         A_bound = (0, 20)
@@ -8,22 +9,28 @@ def test_extract_config_defaults():
         D_bound = (0, 20)
         Ssite_bound = (0, 20)
         Dsite_bound = (0, 20)
+
         fix_A = None
         fix_B = 1
         fix_C = 1
         fix_D = 1
         fix_Ssite = None
         fix_Dsite = 1
+
         fix_t = ''
         bootstraps = 0
         profile_start = 0
         profile_end = 100
         profile_step = 10
-        out_dir = "test_output"
 
-    bounds, fixed, tfixed = extract_config(Args())
+        input_excel = "tests/mock.xlsx"
+
+    config = extract_config(Args())
+
+    bounds = config['bounds']
+    fixed = config['fixed_params']
+    tfixed = config['time_fixed']
+
     assert bounds["A"] == (0, 20)
-    assert bounds["Ssite"] == (0, 20)
     assert fixed["B"] == 1
-    assert fixed["Ssite"] is None
     assert tfixed == {}
