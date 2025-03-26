a0264c85df008a8573eb30941019f1fba706983d
Abhinav Mishra <mishraabhinav36@gmail.com>
2025-03-26 20:57:18 +0100

    testing workflow - series of tests

--- __init__.py
+++ __init__.py
@@ -1,8 +1,4 @@
 """
 PhosKinTime: ODE-based modeling of phosphorylation dynamics using kinase information and time-series mass spectrometry data.
 """
-
 __version__ = "0.1.0"
-
-# Optional: expose commonly used functionality (if used like `from phoskinodex import ...`)
-from .config import config
