023cf5bc032a8b2887967bb8dc3ba77f5b3f0ba3
Abhinav Mishra <mishraabhinav36@gmail.com>
2025-03-26 21:56:19 +0100

    fixes - püath problem

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
 import pandas as pd
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
 def dummy_time_points():
