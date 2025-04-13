87091940630a58113c2ead55165b41982c415564
Abhinav Mishra <mishraabhinav36@gmail.com>
2025-04-13 23:34:59 +0200

    Added mapping for TF - mRNA - Psite _ Kinase.
    minor change in .gitignore

---
+++ processing/postoptmerge.py
@@ -0,0 +1,69 @@
+import pandas as pd
+
+# Function to read TF-mRNA optimization results file sheet 'Alpha Values'
+# and return mRNA for each TF where Values was not zero
+def map_optimization_results(file_path):
+    # Read the Excel file
+    tfopt_file = pd.ExcelFile(file_path)
+
+    # Read the 'Alpha Values' sheet
+    df = pd.read_excel(tfopt_file, sheet_name='Alpha Values')
+
+    # Filter the DataFrame for non-zero values
+    non_zero_df = df[df['Value'] != 0]
+
+    # Filter for positive values
+    # non_zero_df = df[df['Value'] > 0]
+
+    # Take all values without filtering
+    # non_zero_df = df
+
+    # Filter very small values out
+    # non_zero_df = non_zero_df[non_zero_df['Value'] > 0.0001]
+
+    # Extract the mRNA for each TF where Values was not zero
+    result = non_zero_df[['TF', 'mRNA']]
+
+    # Display value along with TF and mRNA
+    # result['Value'] = str(non_zero_df['Value'])
+
+    result = result.groupby('TF').agg(lambda x: ', '.join(x)).reset_index()
+
+    # List number of mRNA for each TF
+    # result['mRNA_Count'] = result['mRNA'].apply(lambda x: len(x.split(',')))
+
+    # Read another csv file which has TF aka GeneID and Psites and Kinases to merge with the result
+    kinopt_file = pd.read_csv('raw/input2.csv')
+    df2 = kinopt_file.rename(columns={'GeneID': 'TF'})
+    merged_df = pd.merge(result, df2, on='TF', how='left')
+
+    # Remove {} from the Kinases column with format {kinase1, kinase2, kinase3}
+    merged_df['Kinase'] = merged_df['Kinase'].astype(str)
+    merged_df['Kinase'] = merged_df['Kinase'].str.replace(r'{', '', regex=True)
+    merged_df['Kinase'] = merged_df['Kinase'].str.replace(r'}', '', regex=True)
+
+    # Remove any extra spaces
+    merged_df['Kinase'] = merged_df['Kinase'].str.replace(r'\s+', '', regex=True)
+
+    # Delete the rows where Psite doesn't match Psite in input2
+    merged_df = merged_df[merged_df['Psite'].isin(df2['Psite'])]
+
+    # Empty cells in mRNA column if they are repeateed in the next row
+    merged_df['mRNA'] = merged_df['mRNA'].where(merged_df['mRNA'] != merged_df['mRNA'].shift(), '')
+
+    merged_df = merged_df.drop_duplicates()
+    merged_df = merged_df.reset_index(drop=True)
+
+    return merged_df
+
+if __name__ == "__main__":
+    file_path = '../data/tfopt_results.xlsx'  # Replace with your file path
+    mapped_df = map_optimization_results(file_path)
+    mapped_df.to_csv('mapped_TF_mRNA_phospho.csv', index=False)
+
+""" 
+PAK2 doesn't exist in CollectTRI (so also not in input4.csv) but it is  
+in the GeneID column of the phospho interaction file. 
+No mRNA-TF estimation is possible for PAK2.  
+Hence, it is not included in the output mapped file for ODE modelling.
+"""
